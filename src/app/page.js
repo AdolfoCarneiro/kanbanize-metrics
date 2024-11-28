03a934cf7aced2660a16e7314f37bbb8969de1c3
ChrisCoy <christopherleegs@outlook.com>
2024-11-28 18:45:26 -0300

    não sei oque eu faço, a minha vida é uma luta sem fim

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,35 @@
-'use client'
-import styles from "./page.module.css";
+"use client";
+import { Card } from "@/components/card/card";
 import { useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home({ props }) {
-
   const workItemTypes = {
-    release: { id: 16, name: "Release" },
-    task: { id: 3, name: "Task" },
-    userStory: { id: 2, name: "User Story" },
-    bug: { id: 1, name: "Bug" },
-    spike: { id: 5, name: "Spike" },
-    epic : { id: 6, name: "Epic" }
+    16: { name: "Release" },
+    3: { name: "Task" },
+    2: { name: "User Story" },
+    1: { name: "Bug" },
+    5: { name: "Spike" },
+    6: { name: "Epic" },
   };
+  // const workItemTypes = {
+  //   release: { id: 16, name: "Release" },
+  //   task: { id: 3, name: "Task" },
+  //   userStory: { id: 2, name: "User Story" },
+  //   bug: { id: 1, name: "Bug" },
+  //   spike: { id: 5, name: "Spike" },
+  //   epic: { id: 6, name: "Epic" },
+  // };
 
+  const [tiposSelecionado, setTiposSelecionado] = useState([]);
   const [dataInicio, setDataInicio] = useState(new Date(2024, 10, 1));
   const [dataFim, setDataFim] = useState(new Date(2024, 10, 30));
-  const dados = props.data
-  .filter((item) => {
+  const dados = props.data.filter((item) => {
     return (
-      ((new Date(item.last_end_time_end_time) >= dataInicio && new Date(item.last_end_time) <= dataFim) ||
-        (new Date(item.first_end_time) >= dataInicio && new Date(item.first_end_time) <= dataFim))
+      (new Date(item.last_end_time_end_time) >= dataInicio &&
+        new Date(item.last_end_time) <= dataFim) ||
+      (new Date(item.first_end_time) >= dataInicio &&
+        new Date(item.first_end_time) <= dataFim)
     );
   });
 
@@ -32,7 +42,16 @@ export default function Home({ props }) {
 
   console.log(dados);
 
-  const throwputSize = dados.reduce((accumulator, currentValue) => accumulator + currentValue.size, 0);
+  const filteredData = tiposSelecionado.length !== 0 ? dados.filter((item) =>
+    tiposSelecionado.includes(item.type_id)
+  ) : dados;
+
+  const throwputSize = filteredData.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.size,
+    0
+  );
+
+  console.log(tiposSelecionado);
 
   const releases = dados.filter((item) => item.type_id == 16).length;
 
@@ -52,10 +71,12 @@ export default function Home({ props }) {
                 border: "none",
                 borderRadius: "5px",
                 cursor: "pointer",
-                color: "black"
+                color: "black",
               }}
             >
-              {new Date(2024, index).toLocaleString("default", { month: "long" })}
+              {new Date(2024, index).toLocaleString("default", {
+                month: "long",
+              })}
             </button>
           ))}
         </div>
@@ -82,6 +103,55 @@ export default function Home({ props }) {
             />
           </label>
         </div>
+        <br />
+        <div>
+          <label>
+            <strong>Tipos:</strong>{" "}
+            <select
+              // value={tiposSelecionado}
+              value={"default"}
+              onChange={(e) => {
+                const alreadySelected = tiposSelecionado.includes(
+                  parseInt(e.target.value)
+                );
+
+                if (alreadySelected) {
+                  setTiposSelecionado(
+                    tiposSelecionado.filter(
+                      (item) => item !== parseInt(e.target.value)
+                    )
+                  );
+                } else {
+                  setTiposSelecionado([
+                    ...tiposSelecionado,
+                    parseInt(e.target.value),
+                  ]);
+                }
+              }}
+            >
+              <option value="default">Selecione as opções</option>
+              {Object.keys(workItemTypes).map((key, i) => (
+                <option
+                  key={key}
+                  value={key}
+                  style={{
+                    background: tiposSelecionado.includes(parseInt(key))
+                      ? "purple"
+                      : "unset",
+                  }}
+                >
+                  {workItemTypes[key].name}
+                </option>
+              ))}
+            </select>
+            {/* <input
+              type="date"
+              value={dataFim.toISOString().split("T")[0]}
+              onChange={(e) => setDataFim(new Date(e.target.value))}
+            /> */}
+          </label>
+        </div>
+
         <div>
           <h4>Datas Selecionadas:</h4>
           <p>
@@ -92,13 +162,21 @@ export default function Home({ props }) {
           </p>
         </div>
       </div>
-      );
-      <main className={styles.main}>
-        <h1 className={styles.title}>Kanbanize Metrics</h1>
-        <p>throwput: {dados.length}</p>
-        <p>story ponits: {throwputSize}</p>
-        <p>story ponits: {releases}</p>
-      </main>
+      <div style={{display: "flex", flexDirection: "column", gap: 64}}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Metricas Gerais</h1>
+          <div className={styles["cards-container"]}>
+            <Card label="releases" value={releases} />
+          </div>
+        </main>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Kanbanize Metrics</h1>
+          <div className={styles["cards-container"]}>
+            <Card label="throwput" value={filteredData.length} />
+            <Card label="story points" value={throwputSize} />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
