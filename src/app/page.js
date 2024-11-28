cf10dab5d07ccff939119eb685e3cd8e1df9487a
adolfo carneiro <adolfocarneiro555@gmail.com>
2024-11-28 16:47:05 -0300

    feat: filtro

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,100 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home({ props }) {
+
+  const workItemTypes = {
+    release: { id: 1, name: "Release" },
+    task: { id: 2, name: "Task" },
+    userStory: { id: 3, name: "User Story" },
+    bug: { id: 4, name: "Bug" },
+  };
+
+  const [dataInicio, setDataInicio] = useState(new Date(2024, 10, 1));
+  const [dataFim, setDataFim] = useState(new Date(2024, 10, 30));
+  const dados = props.data.filter((item) => {
+    return (
+      ((new Date(item.last_end_time_end_time) >= dataInicio && new Date(item.last_end_time) <= dataFim) ||
+        (new Date(item.first_end_time) >= dataInicio && new Date(item.first_end_time) <= dataFim)) &&
+      (item.type_id == workItemTypes.task.id || item.type_id == workItemTypes.userStory.id)
+    );
+  });
+
+  const setMonthDates = (year, month) => {
+    const firstDay = new Date(year, month, 1);
+    const lastDay = new Date(year, month + 1, 0);
+    setDataInicio(firstDay);
+    setDataFim(lastDay);
+  };
+
+  console.log(dados);
+
+  const throwputSize = dados.reduce((accumulator, currentValue) => accumulator + currentValue.size, 0);
+
+  console.log(throwputSize);
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div>
+        <h3>Selecionar Mês</h3>
+        <div>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <button
+              key={index}
+              onClick={() => setMonthDates(2024, index)} // Define o ano e o mês
+              style={{
+                margin: "5px",
+                padding: "10px",
+                backgroundColor: "yellow",
+                border: "none",
+                borderRadius: "5px",
+                cursor: "pointer",
+                color: "black"
+              }}
+            >
+              {new Date(2024, index).toLocaleString("default", { month: "long" })}
+            </button>
+          ))}
         </div>
+        <h4>Ou selecione datas específicas:</h4>
+
+        {/* Inputs para selecionar qualquer data */}
+        <div style={{ margin: "10px 0" }}>
+          <label>
+            <strong>Data Início:</strong>{" "}
+            <input
+              type="date"
+              value={dataInicio.toISOString().split("T")[0]}
+              onChange={(e) => setDataInicio(new Date(e.target.value))}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            <strong>Data Fim:</strong>{" "}
+            <input
+              type="date"
+              value={dataFim.toISOString().split("T")[0]}
+              onChange={(e) => setDataFim(new Date(e.target.value))}
+            />
+          </label>
+        </div>
+        <div>
+          <h4>Datas Selecionadas:</h4>
+          <p>
+            <strong>Data Início:</strong> {dataInicio.toLocaleDateString()}
+          </p>
+          <p>
+            <strong>Data Fim:</strong> {dataFim.toLocaleDateString()}
+          </p>
+        </div>
+      </div>
+      );
+      <main className={styles.main}>
+        <h1 className={styles.title}>Kanbanize Metrics</h1>
+        <p>throwput: {dados.length}</p>
+        <p>story ponits: {throwputSize}</p>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
