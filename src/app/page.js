2895afaf9a034514ad4a0c9bac5ff1a27390cbab
ChrisCoy <christopherleegs@outlook.com>
2024-12-06 10:01:19 -0300

    fix data

--- src/app/page.js
+++ src/app/page.js
@@ -30,10 +30,10 @@ export default function Home({ props }) {
   const [dataFim, setDataFim] = useState(new Date(2024, 10, 30, 23, 59, 59));
   const dados = props.data.filter((item) => {
     return (
-      new Date(item.first_end_time) >= dataInicio &&
-      new Date(item.first_end_time) <= dataFim
-      // (new Date(item.last_start_time) >= dataInicio &&
-      //   new Date(item.last_end_time) <= dataFim)
+      (new Date(item.last_end_time) >= dataInicio &&
+        new Date(item.last_end_time) <= dataFim) ||
+      (new Date(item.first_end_time) >= dataInicio &&
+        new Date(item.first_end_time) <= dataFim)
     );
   });
 
